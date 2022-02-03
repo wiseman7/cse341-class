f1b88850bb3a7f62fa6e0c4eb50c53b220367273
wiseman7 <wis16002@byui.edu>
2022-02-03 14:14:41 -0700

    push to heroku here

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://adminuser:user1@cluster0.f9ktm.mongodb.net/shop';
+
 
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
@@ -24,7 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'my secret', 
     resave: false, 
-    saveUninitialized: false 
+    saveUninitialized: false,
+    store: store 
 }));
 
 app.use((req, res, next) => {
@@ -44,7 +52,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        'mongodb+srv://adminuser:user1@cluster0.f9ktm.mongodb.net/shop?retryWrites=true&w=majority'
+        MONGODB_URI
     )
     .then(result => {
         User.findOne().then(user => {
