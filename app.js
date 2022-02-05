27a55563ff2372b0aa2bce698a889f6b7b37d04d
wiseman7 <wis16002@byui.edu>
2022-02-04 18:22:23 -0700

    route authentication

--- app.js
+++ app.js
@@ -1,78 +1,69 @@
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-const MONGODB_URI = 'mongodb+srv://adminuser:user1@cluster0.f9ktm.mongodb.net/shop';
-
+const MONGODB_URI =
+  "mongodb+srv://adminuser:user1@cluster0.f9ktm.mongodb.net/shop";
 
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
+  uri: MONGODB_URI,
+  collection: "sessions",
 });
 
-app.set('view engine', 'ejs');
-app.set('views', 'views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
-app.use(bodyParser.urlencoded({extended: false})); // This will parse the body, goes through the req,res, and next
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: 'my secret', 
-    resave: false, 
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+); // This will parse the body, goes through the req,res, and next
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
     saveUninitialized: false,
-    store: store 
-    })
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
-    if (!req.session.user) {
-        return next();
-    }
-    User.findById(req.session.user._id)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
-
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        MONGODB_URI
-    )
-    .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User ({
-                    name: 'Kelsey',
-                    email: 'kelsey@test.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        })
-        app.listen(PORT);
-    }).catch(err => {
-        console.log(err);
-    });
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
+router.get('/signup', authController.getSignup)
+
 router.post('/login', authController.postLogin);
+
+router.get('signup', authController.postSignup)
 
 router.post('/logout', authController.postLogout);
 
