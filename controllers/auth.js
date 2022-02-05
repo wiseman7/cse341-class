974732b72b6e9f53ed68b5829ce5037da0a6d0f6
wiseman7 <wis16002@byui.edu>
2022-02-04 19:21:15 -0700

    flash error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,18 +3,30 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
+    errorMessage: message
   });
 };
 
@@ -24,6 +36,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
+        req.flash('error', 'Invalid email or password.');
         return res.redirect('/login');
       }
       bcrypt
@@ -36,7 +49,8 @@ exports.postLogin = (req, res, next) => {
               console.log(err);
               return res.redirect('/');
             });
-        }
+          }
+        req.flash('error', 'Invalid email or password.');
         res.redirect('/login');
        })
       .catch(err => {
@@ -46,7 +60,7 @@ exports.postLogin = (req, res, next) => {
       
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
@@ -58,6 +72,7 @@ exports.postSignup = (req, res, next) => {
   })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash('error', 'E-mail exists already, please pick a different one.');
         return res.redirect("/signup");
       }
       return bcrypt
