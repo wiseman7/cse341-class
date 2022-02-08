863c1482f9c199c2fc7178c36e63fa63985853d5
wiseman7 <wis16002@byui.edu>
2022-02-08 15:49:52 -0700

    added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const {
+    check,
+    body
+} = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -8,9 +13,54 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup)
 
-router.post('/login', authController.postLogin);
+router.post('/login', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email address.')
+            .normalizeEmail()
+        ,
+        body('password', 'Password is not valid.').isLength({min: 5}).isAlphanumeric().trim()
+    ]
+    ,authController.postLogin);
 
-router.get('signup', authController.postSignup)
+router.get('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('PLease enter a valid Email')
+            .custom((value, {
+                req
+            }) => {
+                // if (value === 'test@test.com') {
+                //     throw new Error('This email addres is forbidden.');
+                // }
+                // return true;
+                return User.findOne({ email: value })
+                    .then((userDoc) => {
+                      if (userDoc) {
+                        return Promise.reject('E-Mail exists already, please pick a different one.');
+                      }
+                    });
+                })
+                .normalizeEmail(),
+        body(
+            'password', 
+            'Please enter a password with only numbers and text and at least 5 characters.'
+        )
+            .isLength({min: 5})
+            .isAlphanumeric()
+            .trim(),
+        body('confirmedPassword')
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error('Passwords have to match.')
+            }
+            return true;
+        })
+    ],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
@@ -23,4 +73,4 @@ router.get('/reset/:token', authController.getNewPassword);
 router.post('/new-password', authController.postNewPassword);
 
 
-module.exports = router; 
+module.exports = router;
